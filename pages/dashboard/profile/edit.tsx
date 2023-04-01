3d631995103757f2834842581c02158275795232
Júlio Pinto <pinto.julioj03@gmail.com>
2023-04-01 13:10:15 +0100

    feat: profile page

---
+++ pages/dashboard/profile/edit.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Button, Form, Input, Row, Space, Layout, Avatar } from "antd";
+import { UploadOutlined, UserOutlined } from "@ant-design/icons";
+import DashboardLayout from "../../../components/DashboardLayout/DashboardLayout";
+
+function EditUser() {
+    const layout = {
+        labelCol: { span: 8 },
+        wrapperCol: { span: 16 },
+    };
+
+    /* eslint-disable no-template-curly-in-string */
+    const validateMessages = {
+        required: "${label} is required!",
+    };
+    /* eslint-enable no-template-curly-in-string */
+
+    const onFinish = (values: any) => {
+        console.log(values);
+    };
+
+    return (
+        <DashboardLayout>
+            <Layout>
+                <Form
+                    {...layout}
+                    name="nest-messages"
+                    className="mt-8 py-1"
+                    style={{ maxWidth: 800 }}
+                    onFinish={onFinish}
+                    validateMessages={validateMessages}
+                >
+                    <Row className="text-extrabold text-3xl my-3 mx-6">User Profile</Row>
+                    <Space />
+                    <Form.Item
+                        name={["user", "name"]}
+                        label="Name"
+                        rules={[{ required: true }]}
+                    >
+                        <Input.TextArea />
+                    </Form.Item>
+                    <Form.Item
+                        name={["user", "email"]}
+                        label="Email"
+                        rules={[{ required: true }]}
+                    >
+                        <Input.TextArea />
+                    </Form.Item>
+                    <Form.Item name={["user", "username"]} label="Username">
+                        <Input.TextArea />
+                    </Form.Item>
+                    <Form.Item name={["user", "introduction"]} label="Avatar">
+                        <Space wrap size={16}>
+                            <Avatar shape="square" size={64} icon={<UserOutlined />} />
+                        </Space>
+                        <div className="mt-3" > <Button icon={<UploadOutlined />}>Upload</Button> </div>
+                    </Form.Item>
+                    <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 3 }}>
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Layout>
+        </DashboardLayout>
+    );
+}
+
+
+export default EditUser;
